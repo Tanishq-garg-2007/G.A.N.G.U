f315eaf66985aed45e54b148080f4d445bf4a2cf
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-10-09 11:13:30 +0530

    Added File Upload Function

--- src/agents/main.py
+++ src/agents/main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import sys
-from agents.crew import MedicalCrew
+from agents.crew import SpeechCrew, FileCrew
 from agents.speech_to_text import speech_to_text
+from agents.file_to_text import file_to_text
 
 def run(statement=None):
     """
     Run the crew.
     """
-    statement = speech_to_text(duration=5) 
-    print(f"Transcribed: {statement}")
-    
-    inputs = {
-        "statement": statement
-    }
-    
-    result = MedicalCrew().crew().kickoff(inputs=inputs)
+    choice = input("Enter Your Choice: ")
+
+    if choice == "1":
+        statement = speech_to_text(duration=5)
+        result = SpeechCrew().crew().kickoff(inputs={"statement": statement})
+    elif choice == "2":
+        statement = file_to_text()
+        result = FileCrew().crew().kickoff(inputs={"statement": statement})
+    else:
+        return "Please Enter A Valid Choice"
+
     return result
